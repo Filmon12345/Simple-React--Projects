384acaf836390cc4f9de5c9670b661ac0a33722f
Philodev <zimilifila3676@gmail.com>
2024-01-15 00:19:55 -0800

    adding some functionality

--- Multiselection/src/components/accordian/index.jsx
+++ Multiselection/src/components/accordian/index.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react"
+import data from "./data"
 
+export default function Accordian(){
+    const [selected ,setSelelcted] = useState(null);
+const handlesingleSelection=(getid)=>{
+    console.log(getid)
+}
+    return( 
+    <div className="wrapper">
+        <div className="accordian" >
+            {data && data.length > 0 ?(
+            
+            data.map((dataItem)=>(
+                <div className="item" key={dataItem.id}>
+                    <div onClick={()=>handlesingleSelection(dataItem.id)} className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
 
-export default function Accordian
+                </div>
+            ))
+            )
+            : <div>no data found</div>
+            }
+        </div>
+
+    </div>
+);
+}
