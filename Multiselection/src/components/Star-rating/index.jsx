05d1d1bbacbddb1f9d7a696b7af46961c58e9d20
Philodev <zimilifila3676@gmail.com>
2024-01-16 00:50:01 -0800

    final of project 3 project

--- Multiselection/src/components/Star-rating/index.jsx
+++ Multiselection/src/components/Star-rating/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import './styles.css' ;
 
-export default function StarRating({ noOfStars = 5}){
+export default function StarRating({noOfStars = 5}){
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
   
--- Multiselection/src/components/random-color/index.jsx
+++ Multiselection/src/components/random-color/index.jsx
@@ -34,9 +34,8 @@ else handleCreateRandomHexColor();
       style={{
         display: "flex",
         justifyContent: "center",
-
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         background: color,
       }}
     >
