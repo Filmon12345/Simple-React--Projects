68857adef36f215311729dcc9c1a44aa7ff6b115
Philodev <zimilifila3676@gmail.com>
2024-01-16 00:31:19 -0800

    install react icons

--- Multiselection/src/App.jsx
+++ Multiselection/src/App.jsx
@@ -1,18 +1,20 @@
-import Accordian from  "./components/accordian"
-import './App.css'
-import RandomColor from "./components/random-color"
+import Accordian from "./components/accordian";
+import "./App.css";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/Star-rating";
 function App() {
- 
-
   return (
     <>
-    <h3>PROJECT 1 ACCORDIAN</h3>
-    <Accordian /> 
+      <h3>PROJECT 1: ACCORDIAN</h3>
+      <Accordian />
+
       <h3>PROJECT 2: RANDOM COLOR GENERATOR</h3>
-      
-    <RandomColor />
+      <RandomColor />
+
+      <h3>PROJECT 3: STAR RATING</h3>
+      <StarRating noOfStars={10} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
