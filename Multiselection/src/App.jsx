62bd44f5190bad3ec5dbfd130cb016fa4f35bc12
Philodev <zimilifila3676@gmail.com>
2024-01-15 23:46:21 -0800

    Progect 2 Generate Random rgb and Hex  Color

--- Multiselection/src/App.jsx
+++ Multiselection/src/App.jsx
@@ -1,13 +1,16 @@
 import Accordian from  "./components/accordian"
 import './App.css'
-
+import RandomColor from "./components/random-color"
 function App() {
  
 
   return (
     <>
+    <h3>PROJECT 1 ACCORDIAN</h3>
     <Accordian /> 
+      <h3>PROJECT 2: RANDOM COLOR GENERATOR</h3>
       
+    <RandomColor />
     </>
   )
 }
---
+++ Multiselection/src/components/random-color/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./style.css";
+export default function RandomColor() {
+  const [typeofColor, setTypeofColor] = useState("hex");
+  const [color, setColor] = useState("");
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  }
+
+  function handleCreateRandomRgbColor() {
+    const r = randomColorUtility(256);
+   const g = randomColorUtility(256);
+  const b = randomColorUtility(256);
+  setColor(`rgb(${r}, ${g},${b})`);
+  }
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+
+        width: "100vw",
+        height: "100vh",
+        background: color,
+      }}
+    >
+      <div>
+        <button className="Hex-btn" onClick={() => setTypeofColor("hex")}>
+          Select HEX Color
+        </button>
+        <button
+          className="Random-btn"
+          onClick={
+            typeofColor === "hex"
+              ? handleCreateRandomHexColor
+              : handleCreateRandomRgbColor
+          }
+        >
+          {" "}
+          Generate
+        </button>
+        <button className="Rgb-btn" onClick={() => setTypeofColor("rgb")}>
+          Select RGB Color
+        </button>
+        <div className="display">
+          <h3>{typeofColor === 'rgb' ? 'RGB COLOR' : 'HEX COLOR' }</h3>
+          <h1>{color}</h1>
+      </div>
+      </div>
+     
+    </div>
+  );
+}
